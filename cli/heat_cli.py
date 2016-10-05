8d87524a6d39ea75b2abb9ef20fdcbd4a6ac8924
jgarnier <je.garnier19@gmail.com>
2016-10-05 15:24:27 +0200

    minor issue on checking pr_key

--- cli/heat_cli.py
+++ cli/heat_cli.py
@@ -115,7 +115,7 @@ def setup_flavor_templates(flavor):
         yaml.dump(pnda_env, outfile, default_flow_style=False)        
     shutil.copytree('../../scripts', './scripts')
     shutil.copy('../../deploy', './')
-    if(os.path.isfile('../../pr_key'))
+    if os.path.isfile('../../pr_key'):
         shutil.copy('../../pr_key', './')
 
 def create_cluster(args):
