8193af04453602d12a225e8fed2f717061b554ce
trsmith2 <trsmith2@cisco.com>
2017-06-22 18:56:51 +0100

    Support for simpler networking
    Makes 'create network' a special case of
    electing to use neutron networking in general

--- cli/heat_cli.py
+++ cli/heat_cli.py
@@ -150,7 +150,7 @@ def setup_flavor_templates(flavor, cname, is_bare, fs_type, zknodes, kafkanodes,
         template_vars['create_bastion'] = 0
     else:
         template_vars['neutron_network'] = 1
-        template_vars['create_network'] = 1
+        template_vars['create_network'] = 0
         template_vars['create_volumes'] = 1
         template_vars['create_bastion'] = 1
 
