eb70fb46422768404d1c1382663e3c108889373c
Jeremie Garnier <je.garnier19@gmail.com>
2017-08-02 15:01:33 +0200

    tests for identifying apt issue

--- scripts/package_install.sh
+++ scripts/package_install.sh
@@ -7,7 +7,9 @@ DISTRO=$(cat /etc/*-release|grep ^ID\=|awk -F\= {'print $2'}|sed s/\"//g)
 if [ "x$DISTRO" == "xubuntu" ]; then
   export DEBIAN_FRONTEND=noninteractive
   # give the local mirror the first priority 
+  cp /etc/apt/sources.list /tmp/apt.sources.list.avant
   sed -i "1ideb $PNDA_MIRROR/mirror_deb/ ./" /etc/apt/sources.list
+  cp /etc/apt/sources.list /tmp/apt.sources.list.apres
   wget -O - $PNDA_MIRROR/mirror_deb/pnda.gpg.key | apt-key add -
   (curl -L 'https://archive.cloudera.com/cm5/ubuntu/trusty/amd64/cm/archive.key' | apt-key add - ) && echo 'deb [arch=amd64] https://archive.cloudera.com/cm5/ubuntu/trusty/amd64/cm/ trusty-cm5.9.0 contrib' > /etc/apt/sources.list.d/cloudera-manager.list
   (curl -L 'http://repo.saltstack.com/apt/ubuntu/14.04/amd64/archive/2015.8.11/SALTSTACK-GPG-KEY.pub' | apt-key add - ) && echo 'deb [arch=amd64] http://repo.saltstack.com/apt/ubuntu/14.04/amd64/archive/2015.8.11/ trusty main' > /etc/apt/sources.list.d/saltstack.list
